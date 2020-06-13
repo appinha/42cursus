53e101c85c5c33e2045514aef0914af6e0626df1
appinha <amanda_pinha@hotmail.com>
2020-06-13 15:45:45 -0300

    01 ft_printf - solo project - 882 XP - OK 115% (2020.06.05)

---
+++ 01-ft_printf/ft_printf_utils_2.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils_2.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/19 12:25:19 by apuchill          #+#    #+#             */
+/*   Updated: 2020/05/28 01:47:45 by apuchill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t srcsize;
+	size_t i;
+
+	if (!dst || !src)
+		return (0);
+	srcsize = ft_strlen(src);
+	i = 0;
+	if (dstsize != 0)
+	{
+		while (src[i] != '\0' && i < (dstsize - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (srcsize);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t c;
+	size_t d;
+
+	if (dstsize <= ft_strlen(dst))
+		return (dstsize + ft_strlen(src));
+	c = ft_strlen(dst);
+	d = 0;
+	while (src[d] != '\0' && c + 1 < dstsize)
+	{
+		dst[c] = src[d];
+		c++;
+		d++;
+	}
+	dst[c] = '\0';
+	return (ft_strlen(dst) + ft_strlen(&src[d]));
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	size_t	size;
+	size_t	i;
+
+	size = ft_strlen(s1) + 1;
+	if (!(s2 = (char *)malloc(size)))
+		return (0);
+	i = 0;
+	while (i < size)
+	{
+		((unsigned char *)s2)[i] = ((unsigned char *)s1)[i];
+		i++;
+	}
+	return (s2);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2 ||
+		!(new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*new_str;
+	size_t	i;
+	size_t	j;
+
+	if (!s || !(new_str = (char *)malloc(len + 1)))
+		return (0);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+		new_str[j++] = s[i++];
+	new_str[j] = '\0';
+	return (new_str);
+}
